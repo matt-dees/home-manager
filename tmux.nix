ab14c4cacc9571e6c9add629de4d44d12880e188
Matt Dees <mattdees.cs@gmail.com>
2025-03-22 09:02:25 -0700

    Getting started with home-manager

---
+++ tmux.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+{
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    plugins = with pkgs;
+      [
+        tmuxPlugins.better-mouse-mode
+      ];
+    extraConfig = ''
+      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+      set -g default-terminal "tmux-256color"
+      set -ga terminal-overrides ",alacritty:RGB"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
+      bind -T copy-vode-vi Enter send-keys -X copy-pipe-and-cancel "xsel -i --clipboard"
+
+      # Mouse works as expected
+      set-option -g mouse on
+      # easy-to-remember split pane commands
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+    '';
+  };
+}
